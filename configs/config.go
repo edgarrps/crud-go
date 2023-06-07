3c79b9fec13dd8c828790fd3a67e2343b058c128
edgarrps <edgarrps@gmail.com>
2023-06-07 12:16:16 -0400

    config db and api

---
+++ configs/config.go
@@ -0,0 +1,28 @@
+package configs
+
+import "github.com/spf13/viper"
+
+var cfg *config
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	Item     string
+	Amount   int
+	Database string
+}
+
+func init() {
+	viper.SetDefault("api.port", "9000")
+	viper.SetDefault("database.host", "localhost")
+	viper.SetDefault("database.port", "5432")
+}
