6e0db00131b2ec4d0de9c66101cd72d68331f45e
edgarrps <edgarrps@gmail.com>
2023-06-11 00:29:21 +0000

    get.go

--- models/get.go
+++ models/get.go
@@ -1,5 +1,16 @@
 package models
 
+import "github.com/edgarrps/crud-go/db"
+
 func Get(id int64) (todo Todo, err error) {
-return
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	row := conn.QueryRow(`SELECT * FROM todos WHERE id=$1`, id)
+
+	err = row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+	return
 }
