808c41064ce8c491f78436779d4e235e91db158a
edgarrps <edgarrps@gmail.com>
2023-06-13 20:32:51 -0400

    update.go

--- models/update.go
+++ models/update.go
@@ -1,1 +1,17 @@
 package models
+
+import "github.com/edgarrps/crud-go/db"
+
+func Update(id int64, todo Todo) (int64, error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	res, err := conn.Exec(`UPDATE todos SET title=$1, description=$2, done=$3 where id=$4`, todo.Title, todo.Description, todo.Done, todo.ID)
+	if err != nil {
+		return 0, err
+	}
+	return res.RowsAffected()
+}
