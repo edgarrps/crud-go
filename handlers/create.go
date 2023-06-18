82c65a84a54e6fc2ed36bf4ae702ea975e47ac7a
edgarrps <edgarrps@gmail.com>
2023-06-18 16:39:15 -0400

    implementation Create func

--- handlers/create.go
+++ handlers/create.go
@@ -1,7 +1,17 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/edgarrps/crud-go/models"
+)
 
 func Create(w http.ResponseWriter, r *http.Request) {
+	var todo models.Todo
 
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
