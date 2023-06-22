bf400ee8991d5a0dba313841dca217ee6f847e4f
Edgar Santos <68236516+edgarrps@users.noreply.github.com>
2023-06-22 16:32:13 -0400

    Update update.go

--- handlers/update.go
+++ handlers/update.go
@@ -1,1 +1,10 @@
 package handlers
+
+func Update(w http.ResponseWriter, r *http.Request) {
+  id, err := strconv.Atoi(chi.URLParam(r, "id"))
+  if err != nil {
+  log.Printf("Erro ao fazer parse do id: %v", err)
+    http.Error(w, http.StatusInternalServerError), StatusInternalServerError
+    return
+  }
+}
