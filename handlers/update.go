b0c2fe55952d8b773d95f38c887fc4bd341a4338
edgarrps <edgarrps@gmail.com>
2023-06-24 23:08:52 -0400

    update update.go

--- handlers/update.go
+++ handlers/update.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/edgarrps/crud-go/models"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -14,5 +17,30 @@ func Update(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Erro ao fazer parse do id %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
+	}
+
+	var todo models.Todo
+
+	err = json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Update(int64(id), todo)
+	if err != nil {
+		log.Printf("Erro ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if rows > 1 {
+		log.Printf("Error: foram atualizados de %d registros", rows)
+		resp := map[string]any{
+			"Error":   false,
+			"Message": fmt.Sprintf("dados atualizados com sucesso! ID: %d", id),
+		}
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
 	}
 }
