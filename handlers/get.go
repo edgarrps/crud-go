c0330fdd4079b3b8610c61081931fbaba251b37b
edgarrps <edgarrps@gmail.com>
2023-06-27 22:27:13 -0400

    update get.go

--- handlers/get.go
+++ handlers/get.go
@@ -1,7 +1,29 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
 
-func Get(w http.ResponseWriter) {
-	return
+	"github.com/edgarrps/crud-go/models"
+	"github.com/go-chi/chi/v5"
+)
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	todo, err := models.Get(int64(id))
+	if err != nil {
+		log.Printf("Erro ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
 }
