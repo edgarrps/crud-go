38b8a727de3f481fdad5c155eb59558c1ee0f584
edgarrps <edgarrps@gmail.com>
2023-06-28 17:32:22 -0400

    func main

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/edgarrps/crud-go/configs"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/edgarrps/crud-go/configs"
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
 	err := configs.Load()
 	if err != nil {
 		panic(err)
 	}
+
+	r := chi.NewRouter()
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 }
